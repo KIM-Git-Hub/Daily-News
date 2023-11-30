ee93c85df9d4663ac4d1fcd2a9867d282c678d1f
deliciousBanana <tts4419@gmail.com>
2023-12-01 00:09:28 +0900

    Daily News 11/30 完

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -20,11 +20,11 @@ class _DetailScreenState extends State<DetailScreen> {
       appBar: AppBar(
         title: TextButton(
           child: const Text(
-            '뒤로가기',
+            'BACK',
             style: TextStyle(
               fontWeight: FontWeight.bold,
               color: Colors.white,
-              fontSize: 24,
+              fontSize: 20,
             ),
           ),
           onPressed: () {
@@ -36,125 +36,135 @@ class _DetailScreenState extends State<DetailScreen> {
       ),
       body: Column(
         children: [
-          Expanded(child: SingleChildScrollView(
+          Expanded(
+              child: SingleChildScrollView(
             child: Column(
               children: [
                 Container(
                   margin: const EdgeInsets.all(16),
                   child: widget.newsItem['urlToImage'] != null
                       ? Column(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        width: double.infinity,
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(10),
-                            topRight: Radius.circular(10),
-                          ),
-                          child: Image.network(
-                            widget.newsItem['urlToImage'],
-                            errorBuilder: (context, error, stackTrace) {
-                              return Image.asset('assets/no_image.png');
-                            },
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 80,
-                        padding: const EdgeInsets.all(8),
-                        width: double.infinity,
-                        decoration: ShapeDecoration(
-                            color: Colors.black.withOpacity(0.7),
-                            shape: const RoundedRectangleBorder(
-                                borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(10),
-                                    bottomRight: Radius.circular(10)))),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Container(
-                              alignment: Alignment.topLeft,
-                              child: Text(
-                                widget.newsItem['title'],
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14,
+                            SizedBox(
+                              height: 200,
+                              width: double.infinity,
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(10),
+                                  topRight: Radius.circular(10),
                                 ),
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
+                                child: Image.network(
+                                  widget.newsItem['urlToImage'],
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return Image.asset('assets/no_image.png');
+                                  },
+                                  fit: BoxFit.cover,
+                                ),
                               ),
                             ),
                             Container(
-                              height: 16,
-                              margin: const EdgeInsets.only(top: 5),
-                              alignment: Alignment.bottomRight,
-                              child: Text(
-                                  formatDate(widget.newsItem['publishedAt']),
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                  )),
+                              height: 80,
+                              padding: const EdgeInsets.all(8),
+                              width: double.infinity,
+                              decoration: ShapeDecoration(
+                                  color: Colors.black.withOpacity(0.7),
+                                  shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(10),
+                                          bottomRight: Radius.circular(10)))),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    alignment: Alignment.topLeft,
+                                    child: Text(
+                                      widget.newsItem['title'],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 14,
+                                      ),
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 16,
+                                    margin: const EdgeInsets.only(top: 5),
+                                    alignment: Alignment.bottomRight,
+                                    child: Text(
+                                        formatDate(
+                                            widget.newsItem['publishedAt']),
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        )),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.topLeft,
+                              margin: const EdgeInsets.only(top: 20),
+                              child: widget.newsItem['description'] != null
+                                  ? Text(
+                                      widget.newsItem['description'],
+                                      style: const TextStyle(fontSize: 15),
+                                    )
+                                  : const Text(''),
+                            ),
+                          ],
+                        )
+                      : Column(
+                          children: [
+                            Container(
+                              ///이미지가 없을때
+                              height: 100,
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.black.withOpacity(0.7),
+                                  border:
+                                      Border.all(width: 1, color: Colors.white),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    widget.newsItem['title'],
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.bottomRight,
+                                    margin: const EdgeInsets.only(top: 10),
+                                    child: Text(
+                                      formatDate(
+                                          widget.newsItem['publishedAt']),
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.topLeft,
+                              margin: const EdgeInsets.only(top: 20),
+                              child: widget.newsItem['description'] != null
+                                  ? Text(
+                                      widget.newsItem['description'],
+                                      style: const TextStyle(fontSize: 15),
+                                    )
+                                  : const Text(''),
                             ),
                           ],
                         ),
-                      ),
-                      Container(
-                        alignment: Alignment.topLeft,
-                        margin: const EdgeInsets.only(top: 20),
-                        child: widget.newsItem['description'] != null
-                            ? Text(
-                          widget.newsItem['description'],
-                          style: const TextStyle(fontSize: 15),
-                        )
-                            : const Text(''),
-                      ),
-                    ],
-                  )
-                      : Container(
-                    ///이미지가 없을때
-                    height: 95,
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.7),
-                        border: Border.all(width: 1, color: Colors.white),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Column(
-                      children: [
-                        Text(
-                          widget.newsItem['title'],
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 14,
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 10),
-                          child: Text(
-                            widget.newsItem['publishedAt'],
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 20),
-                          child: widget.newsItem['description'] != null
-                              ? Text(
-                            widget.newsItem['description'],
-                            style: const TextStyle(fontSize: 15),
-                          )
-                              : const Text(''),
-                        ),
-                      ],
-                    ),
-                  ),
                 ),
               ],
             ),
@@ -165,11 +175,10 @@ class _DetailScreenState extends State<DetailScreen> {
                 onPressed: () {
                   launchURL();
                 },
-                child: const Text('원글로 이동')),
+                child: const Text('元のページを開く')),
           )
         ],
       ),
-
     );
   }
 
@@ -181,7 +190,7 @@ class _DetailScreenState extends State<DetailScreen> {
       if (context.mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(
-            content: Text('페이지로 이동할 수 없음'),
+            content: Text('ページが開けません。'),
           ),
         );
       }
